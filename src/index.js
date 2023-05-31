483896b0c0a33e2e9e54b7d8d73ee70c43d1e010
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-31 15:41:40 -0400

    Designate as 'module', ES6/next.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 // initial commit
 
 import { config } from 'dotenv';
+
+config();
+
+import { Client, GatewayIntentBits, Routes, REST } from 'discord.js';
+// @discordjs/rest not needed?
+
+const { TED_BOT_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+

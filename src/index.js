99fa4b9f42124e18f9ea404ad0652472b03cc539
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-31 16:18:00 -0400

    Successful login, register 'Report' application command.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-// initial commit
-
 import { config } from 'dotenv';
+// @discordjs/rest not needed?
 
 config();
 
-import { Client, GatewayIntentBits, Routes, REST } from 'discord.js';
-// @discordjs/rest not needed?
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
 
-const { TED_BOT_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
-
+const { TED_BOT_TOKEN, TED_CLIENT_ID, TED_GUILD_ID } = process.env;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const rest = new REST({ version: '10' }).setToken(TED_BOT_TOKEN);
 
+client.on('ready', () => console.log(`${client.user.tag} has logged in.`));
 
+const commands = [
+    {
+        name: 'Report',
+        type: 2,
+    }
+];
+
+async function main() {
+    try {
+        await rest.put(Routes.applicationGuildCommands(TED_CLIENT_ID, TED_GUILD_ID), {
+            body: commands,
+        });
+        client.login(TED_BOT_TOKEN);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+main();

df1304adcf1bbde7fdadfa1b87237887f5b22595
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-31 17:56:57 -0400

    Successful test of modal popup, application command menu in Discord.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { config } from 'dotenv';
-// @discordjs/rest not needed?
 
 config();
 
-import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { Client, GatewayIntentBits, Routes, TextInputStyle } from 'discord.js';
 import { REST } from '@discordjs/rest';
+import { ActionRowBuilder, ModalBuilder, TextInputBuilder } from '@discordjs/builders';
 
 const { TED_BOT_TOKEN, TED_CLIENT_ID, TED_GUILD_ID } = process.env;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -18,6 +18,30 @@ const commands = [
         type: 2,
     }
 ];
+
+client.on('interactionCreate', (interaction) => {
+    if (interaction.isUserContextMenuCommand()) {
+        console.log(interaction.commandName);
+        if (interaction.commandName === 'Report') {
+            const modal = new ModalBuilder()
+                .setCustomId('reportUserModal')
+                .setTitle('Report a User')
+                .setComponents(
+                    new ActionRowBuilder().setComponents(
+                        new TextInputBuilder()
+                            .setCustomId('reportMessage')
+                            .setLabel('Report Message')
+                            .setStyle(TextInputStyle.Paragraph)
+                            .setRequired(true)
+                            .setMinLength(10)
+                            .setMaxLength(500)
+                    )
+
+                );
+            interaction.showModal(modal);
+        }
+    }
+})
 
 async function main() {
     try {
